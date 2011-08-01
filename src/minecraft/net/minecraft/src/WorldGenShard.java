a55ef13d180cc64ffffca8375710a7a9c5264f0c
Stormweaker <lurthz@hotmail.fr>
2011-08-01 18:54:56 +0200

    Correction génération test
    Signed-off-by: Stormweaker <lurthz@hotmail.fr>

--- src/minecraft/net/minecraft/src/WorldGenShard.java
+++ src/minecraft/net/minecraft/src/WorldGenShard.java
@@ -5,7 +5,7 @@ import java.util.Random;
 
 /* =============BUGS=============
  * 
- * génération uniquement sur l'axe z
+ * 
  * 
  */
 
@@ -18,19 +18,13 @@ public class WorldGenShard extends WorldGenerator
 
 	public boolean generate(World world, Random random, int i, int j, int k) 
 	{
-		int i1 = i + 10;
-		int j1 = j + 20;
-		int k1 = k + 10;
-		for(; i < i1; i++)//axe x
+		for(int longueur = i; longueur < i + 5; longueur++)//axe x
 		{
-			for(; j < j1; j++)//axe y
+			for(int hauteur = j; hauteur < j + 5; hauteur++)//axe y
 			{
-				for(; k < k1; k++)//axe z
+				for(int largeur = k; largeur < k + 5; largeur++)//axe z
 				{
-					System.out.println("i: " + i + " i1: " + i1);
-					System.out.println("j: " + j + " j1: " + j1);
-					System.out.println("k: " + k + " k1: " + k1);
-				    world.setBlockWithNotify(i, j, k, mod_Aelyth.ObliviousDirt.blockID);
+				    world.setBlockWithNotify(longueur, hauteur, largeur, mod_Aelyth.ObliviousDirt.blockID);
 				}
 			}
 		}
