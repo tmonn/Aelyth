136f5973b2d5ade73f6cb02357a4817c1c42dbf6
unknown <Asus@.(none)>
2011-08-04 19:51:19 +0200

    Test génération Sharg - Bug panneaux

--- src/minecraft/net/minecraft/src/WorldGenShard.java
+++ src/minecraft/net/minecraft/src/WorldGenShard.java
@@ -5,7 +5,7 @@ import java.util.Random;
 
 /* =============BUGS=============
  * 
- * 
+ * Les panneaux ce générent contre le mauvais mur
  * 
  */
 
@@ -62,8 +62,8 @@ public class WorldGenShard extends WorldGenerator
 		 * ==================PLACEMENT COFFRE + UN DIAMAND=================== 
 		 */
 		
-	    world.setBlock(i + 2, j + 2, k + 2, Block.chest.blockID); 
-	    TileEntityChest tileentitychest = (TileEntityChest)world.getBlockTileEntity(i + 2, j + 2, k + 2);
+	    world.setBlock(i + 1, j + 1, k + 2, Block.chest.blockID); 
+	    TileEntityChest tileentitychest = (TileEntityChest)world.getBlockTileEntity(i + 1, j + 1, k + 2);
 	    ItemStack item = new ItemStack(Item.diamond, 1);
         tileentitychest.setInventorySlotContents(random.nextInt(tileentitychest.getSizeInventory()), item);
 		
@@ -88,6 +88,110 @@ public class WorldGenShard extends WorldGenerator
         		}
         	}
         }
+        
+        // i = axe x
+        // j = axe y
+        // k = axe z
+        
+        /*
+		 * ==================FEU INTERIEUR + PORTE=================== 
+		 */
+        
+        world.setBlockWithNotify(i + 1, j, k + 1, Block.netherrack.blockID);
+        world.setBlockWithNotify(i + 2, j, k + 1, Block.netherrack.blockID);
+        world.setBlockWithNotify(i + 3, j, k + 1, Block.netherrack.blockID);
+        world.setBlockWithNotify(i + 1, j, k + 3, Block.netherrack.blockID);
+        world.setBlockWithNotify(i + 2, j, k + 3, Block.netherrack.blockID);
+        world.setBlockWithNotify(i + 3, j, k + 3, Block.netherrack.blockID);
+        
+        world.setBlockWithNotify(i + 1, j + 1, k + 1, Block.fire.blockID);
+        world.setBlockWithNotify(i + 2, j + 1, k + 1, Block.fire.blockID);
+        world.setBlockWithNotify(i + 3, j + 1, k + 1, Block.fire.blockID);
+        world.setBlockWithNotify(i + 1, j + 1, k + 3, Block.fire.blockID);
+        world.setBlockWithNotify(i + 2, j + 1, k + 3, Block.fire.blockID);
+        world.setBlockWithNotify(i + 3, j + 1, k + 3, Block.fire.blockID);
+        
+        world.setBlockWithNotify(i + 4, j + 1, k + 2, 0);
+        world.setBlockWithNotify(i + 4, j + 2, k + 2, 0);
+        
+        
+        /*
+		 * ==================FACONNAGE SHARD=================== 
+		 */
+        
+        
+        world.setBlockWithNotify(i + 4, j + 3, k + 4, 0);
+        world.setBlockWithNotify(i + 4, j + 4, k + 4, 0);
+        world.setBlockWithNotify(i + 4, j + 3, k, 0);
+        world.setBlockWithNotify(i + 4, j + 4, k, 0);
+        world.setBlockWithNotify(i + 4, j + 4, k + 1, 0);
+        world.setBlockWithNotify(i + 4, j + 4, k + 3, 0);
+        
+        world.setBlockWithNotify(i + 3, j + 3, k + 4, 0);
+        world.setBlockWithNotify(i + 3, j + 4, k + 4, 0);
+        world.setBlockWithNotify(i + 3, j + 3, k, 0);
+        world.setBlockWithNotify(i + 3, j + 4, k, 0);
+        world.setBlockWithNotify(i + 3, j + 4, k + 1, 0);
+        world.setBlockWithNotify(i + 3, j + 4, k + 3, 0);
+        
+        world.setBlockWithNotify(i + 2, j + 3, k + 4, 0);
+        world.setBlockWithNotify(i + 2, j + 4, k + 4, 0);
+        world.setBlockWithNotify(i + 2, j + 3, k, 0);
+        world.setBlockWithNotify(i + 2, j + 4, k, 0);
+        world.setBlockWithNotify(i + 2, j + 4, k + 1, 0);
+        world.setBlockWithNotify(i + 2, j + 4, k + 3, 0);
+        
+        world.setBlockWithNotify(i + 1, j + 3, k + 4, 0);
+        world.setBlockWithNotify(i + 1, j + 4, k + 4, 0);
+        world.setBlockWithNotify(i + 1, j + 3, k, 0);
+        world.setBlockWithNotify(i + 1, j + 4, k, 0);
+        world.setBlockWithNotify(i + 1, j + 4, k + 1, 0);
+        world.setBlockWithNotify(i + 1, j + 4, k + 3, 0);
+        
+        world.setBlockWithNotify(i, j + 3, k + 4, 0);
+        world.setBlockWithNotify(i, j + 4, k + 4, 0);
+        world.setBlockWithNotify(i, j + 3, k, 0);
+        world.setBlockWithNotify(i, j + 4, k, 0);
+        world.setBlockWithNotify(i, j + 4, k + 1, 0);
+        world.setBlockWithNotify(i, j + 4, k + 3, 0);
+        
+        world.setBlockWithNotify(i + 3, j + 3, k + 1, mod_Aelyth.AelythDirt.blockID);
+        world.setBlockWithNotify(i + 3, j + 3, k + 3, mod_Aelyth.AelythDirt.blockID);
+        
+        world.setBlockWithNotify(i + 2, j + 3, k + 1, mod_Aelyth.AelythDirt.blockID);
+        world.setBlockWithNotify(i + 2, j + 3, k + 3, mod_Aelyth.AelythDirt.blockID);
+        
+        world.setBlockWithNotify(i + 1, j + 3, k + 1, mod_Aelyth.AelythDirt.blockID);
+        world.setBlockWithNotify(i + 1, j + 3, k + 3, mod_Aelyth.AelythDirt.blockID);
+
+        world.setBlockWithNotify(i, j + 3, k + 1, mod_Aelyth.AelythDirt.blockID);
+        world.setBlockWithNotify(i, j + 3, k + 3, mod_Aelyth.AelythDirt.blockID);
+        
+        world.setBlockWithNotify(i, j, k, 0);
+        world.setBlockWithNotify(i, j, k + 4, 0);
+
+        world.setBlockWithNotify(i + 1, j, k, 0);
+        world.setBlockWithNotify(i + 1, j, k + 4, 0);
+        
+        world.setBlockWithNotify(i + 2, j, k, 0);
+        world.setBlockWithNotify(i + 2, j, k + 4, 0);
+        
+        world.setBlockWithNotify(i + 3, j, k, 0);
+        world.setBlockWithNotify(i + 3, j, k + 4, 0);
+        
+        world.setBlockWithNotify(i + 4, j, k, 0);
+        world.setBlockWithNotify(i + 4, j, k + 4, 0);
+        
+        
+//        world.setBlock(i + 1, j + 3, k + 2, Block.signWall.blockID); 
+//	    TileEntitySign tileentitysign = (TileEntitySign)world.getBlockTileEntity(i + 1, j + 3, k + 2);
+//	    
+//        world.setBlock(i + 1, j + 2, k + 2, Block.signWall.blockID); 
+//	    TileEntitySign tileentitysign2 = (TileEntitySign)world.getBlockTileEntity(i + 1, j + 2, k + 2);
+//	    
+//        tileentitysign.signText = new String[] {"Bravo jeune", "pomme, tu viens", "de découvrir", "l'objet qui"};
+//        tileentitysign.signText = new String[] {"va te", "permettre", "d'acceder à", "l'Aelyth !"};
+        
         
 		return true;
 	}
